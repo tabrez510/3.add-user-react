0c6147225f96c815d05474130a0dc24025491fd1
tabrez510 <alamtabrez510@gmail.com>
2024-08-02 19:07:31 +0530

    Error-model-added

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../UI/Card";
+import './UsersList.css'
+
+const UsersList = (props) => {
+
+    const users = props.users;
+    return(
+        <Card className="users">
+            <ul>
+                {users.map((user) => {
+                    return <li key={user.id}>
+                        {user.name} ({user.age} years old)
+                    </li>
+                })}
+            </ul>
+        </Card>
+    )
+}
+
+export default UsersList;
